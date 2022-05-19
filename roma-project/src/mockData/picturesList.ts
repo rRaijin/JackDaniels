67fedeac6938172e6727392609ce0f375b63f682
Roma <rossraijin@gmail.com>
2022-05-19 15:02:19 +0300

    19-05-R

---
+++ roma-project/src/mockData/picturesList.ts
@@ -0,0 +1,17 @@
+const picturesList = [
+    {
+        name: 'picture-1',
+        src: 'https://via.placeholder.com/200x200'
+    },
+    {
+        name: 'picture-2',
+        src: 'https://via.placeholder.com/250x200'
+    },
+    {
+        name: 'picture-3',
+        src: 'https://via.placeholder.com/300x200'
+    }
+];
+
+export default picturesList;
+
---
+++ roma-project/src/pages/Painting.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import fetch from 'cross-fetch';
+
+
+class Painting extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            loaded: false,
+            pictures: []
+        };
+    }
+
+    componentDidMount() {
+        console.log('MOUNTED');
+        setTimeout(() => {
+            fetch('http://jsonplaceholder.typicode.com/posts')
+                .then((res) => {
+                    if (res.status >= 400) {
+                        throw new Error('Bad response from server');
+                    }
+                    return res.json();
+                })
+                .then((data) => {
+                    console.log('data: ', data);
+                    this.setState({
+                        loaded: true,
+                        pictures: data
+                    });
+                })
+                .catch((err) => console.log('Error: ', err));
+        }, 1000);
+    }
+
+    componentWillUnmount() {
+        console.log('component will unmounted');
+    }
+
+    render() {
+        const { loaded, pictures } = this.state;
+
+        return (
+            <div className='pictures-head'>
+                {
+                    loaded ?
+                        pictures.map((item, i) => {
+                            return (
+                                <div key={`pict_prev_${i}`} className='picture-preview'>
+                                    <img src='https://via.placeholder.com/50x50' alt={`picture-preview-${i}`} />
+                                    <h3>{item.title}</h3>
+                                    <p>
+                                        {item.body}
+                                    </p>
+                                </div>
+                            );
+                        }) :
+                        <div>LOADING...</div>
+                }
+            </div>
+        );
+    }
+}
+
+export default Painting;
