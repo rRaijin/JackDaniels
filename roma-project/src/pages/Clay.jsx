e78da1c7b866cf97b21fe2a90bc05bfdc34c6e79
Roma <rossraijin@gmail.com>
2022-05-20 13:11:09 +0300

    20-05-R

--- roma-project/src/pages/Clay.jsx
+++ roma-project/src/pages/Clay.jsx
@@ -8,57 +8,100 @@ class Clay extends React.Component {
 
         this.state= {
             sculptures: [],
+            dataLoaded: false,
+            dataFetched: false,
+            stupidDefence: false,
+            errorMsg: ''
         };
+        this.handleOnClick = this.handleOnClick.bind(this);
+    }
+
+    handleOnClick () {
+        const { dataFetched } = this.state;
+        if (dataFetched) {
+            this.setState({
+                sculptures: [],
+                dataFetched: false
+            });
+        } else {
+            this.setState({
+                dataFetched: true,
+                stupidDefence: true
+            }, () => {
+                setTimeout(() => {
+                    fetch('http://jsonplaceholder.typicode.com/comments?postId=1')
+                        .then((res) => {
+                            if (res.status >= 400) {
+                                console.log('Error');
+                                this.setState({
+                                    errorMsg: 'Server error!'
+                                });
+                            } else {
+                                // const promiseExample = new Promise((resolve, reject) => {
+                                //     console.log('res: ', res);
+                                //     resolve(this.setState({errorMsg: 'BARADA'}));
+                                //     reject(res.json());
+                                // });
+                                // return await promiseExample;
+                                return res.json();
+                            }
+                        })
+                        .then(res => {
+                            this.setState({
+                                sculptures: res,
+                                dataLoaded: true,
+                                stupidDefence: false
+                            });
+                        })
+                        .catch(() => {
+                            this.setState({errorMsg: 'Incorrect format of data'});
+                        });
+                }, 1000);
+            });
+        }
     }
 
     render() {
-        const { sculptures } = this.state;
+        const { dataLoaded, dataFetched, sculptures, stupidDefence, errorMsg } = this.state;
+        const btnText = dataFetched ? 'RESET' : 'GET DATA';
+
         return (
-            <div className='sculptures-list'>
-                {
-                    sculptures.map((item, index) => {
-                        return (
-                            <div key={`item${index}`} className={`sculpture-item${index+1}`}>
-                                <span className='email-id'>{`${item.id+')'} ${item.email}`}</span>
-                                <h3>{item.body}</h3>
-                                <br/>
-                            </div>
-                        );
-                    }) 
-                }
+            <div className=''>
+                <div>
+                    <button
+                        className=''
+                        disabled={stupidDefence}
+                        onClick={this.handleOnClick}>
+                        {btnText}
+                    </button>
+                </div>
+                <div>
+                    {
+                        dataFetched ?
+                            errorMsg === '' ?
+                                <div className='sculptures-list'>
+                                    {   
+                                        dataLoaded ?
+                                            sculptures.map((item, index) => {
+                                                return (
+                                                    <div key={`item${index}`} className={`sculpture-item${index+1}`}>
+                                                        <span className='email-id'>{`${item.id+')'} ${item.email}`}</span>
+                                                        <h3>{item.body}</h3>
+                                                        <br/>
+                                                    </div>
+                                                );
+                                            }) :
+                                            <div>Loading...</div>
+                                    }
+                                </div> :
+                                <div>{errorMsg}</div> :
+                            <div>No data</div>
+                    }
+                </div>
             </div>
         );
     }
 
-    componentDidMount() {
-        console.log('Mounted');
-        fetch('http://jsonplaceholder.typicode.com/comments?postId=1')
-            .then(res => {
-                if (res.status >= 400) {
-                    console.log('Error');
-                } else {
-                    return res.json();
-                }
-            })
-            .then(res => {
-                this.setState({
-                    sculptures: res,
-                    load: true
-                });
-                console.log(res);
-            })
-            .catch(err => {
-                console.log('Error:', err);
-            });
-
-    }
-    componentWillUnmount() {
-        console.log('Unmounted');
-
-    }
-
-
 }
-
 
 export default Clay;
