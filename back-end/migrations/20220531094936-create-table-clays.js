42d889ac0a85fa5c795fa6202bd25d04d8cabe1e
Roma <rossraijin@gmail.com>
2022-05-31 16:17:27 +0300

    some commit

---
+++ back-end/migrations/20220531094936-create-table-clays.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function(db) {
+    db.createTable('clays', {
+        id: { type: 'int', primaryKey: true, autoIncrement: true },
+        name: 'string',
+        description: 'string',
+        pictureFolder: 'string',
+        pictureName: 'string',
+        // createdAt
+    }, () => {
+        console.log('TABLE CLAYS CREATED SUCCESSFULLY!');
+    });
+
+    return null;
+};
+
+exports.down = function(db) {
+  return null;
+};
+
+exports._meta = {
+  "version": 1
+};
---
+++ back-end/migrations/20220531094938-insert-initial-clays.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function(db) {
+    db.insert(
+        'clays',
+        ['name', 'description', 'pictureFolder', 'pictureName'],
+        [
+            'Neptun',
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum officiis recusandae culpa provident fuga quaerat aliquid error aspernatur. Eaque ratione incidunt assumenda porro labore dolorem fuga libero aut repudiandae quae.',
+            'clays',
+            'neptun.png'
+        ]
+    );
+    
+    return null;
+};
+
+exports.down = function(db) {
+  return null;
+};
+
+exports._meta = {
+  "version": 1
+};
